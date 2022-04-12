41edb87acdefd8fcf32746a049eb020999bcb159
Jacob <jacob@goldenneedham.com>
2022-04-11 23:41:43 -0700

    Renaming files

---
+++ PersistantMarshmallow/PersistantMarshmallow.cs
@@ -0,0 +1,28 @@
+﻿using OWML.Common;
+using OWML.ModHelper;
+
+namespace PersistantMarshmallow
+{
+    public class PersistantMarshmallow : ModBehaviour
+    {
+        private void Awake()
+        {
+            // You won't be able to access OWML's mod helper in Awake.
+            // So you probably don't want to do anything here.
+            // Use Start() instead.
+        }
+
+        private void Start()
+        {
+            // Starting here, you'll have access to OWML's mod helper.
+            ModHelper.Console.WriteLine($"{nameof(PersistantMarshmallow)} is loaded!", MessageType.Success);
+
+            ModHelper.HarmonyHelper.AddPrefix<Marshmallow>("Shrivel", typeof(PersistantMarshmallow), "Shrivel");
+        }
+
+        public static bool Shrivel(Marshmallow __instance) {
+            return false;
+        }
+        
+    }
+}
